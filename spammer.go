717aacf2bea23eb4b39541ce12f4892594aabc45
iro23m350 <inyakinro@student.bmstu.ru>
2025-04-15 00:57:50 +0300

    main

---
+++ spammer.go
@@ -0,0 +1,21 @@
+package main
+
+func RunPipeline(cmds ...cmd) {
+
+}
+
+func SelectUsers(in, out chan interface{}) {
+
+}
+
+func SelectMessages(in, out chan interface{}) {
+
+}
+
+func CheckSpam(in, out chan interface{}) {
+	
+}
+
+func CombineResults(in, out chan interface{}) {
+	
+}
